4345ea6d426aba2161da31bf0859f54052abfe9e
ELI DARUSTIYA <114423143+elidarustiya@users.noreply.github.com>
2022-10-27 14:09:23 +0700

    Add files via upload

---
+++ ClassA.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Praktikum-Inheritance_Pemprograman Berorientasi Objek
+ * @author ACER_ELI DARUSTIYA_20210040068_TI21A
+ */
+
+public class ClassA {
+    
+    String var_a = "Variabel A";
+    String var_b = "Variabel B";
+    String var_c = "Variabel C";
+    String var_d = "Variabel D";
+    
+    ClassA(){
+        System.out.println("Konstruktor A dijalankan");
+ }
+}
