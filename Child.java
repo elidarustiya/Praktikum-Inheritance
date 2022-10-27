baa3d4088cf4e7c64b3d80d44b47d4a45bbfc1ea
ELI DARUSTIYA <114423143+elidarustiya@users.noreply.github.com>
2022-10-27 14:07:54 +0700

    Add files via upload

---
+++ Child.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Praktikum-Inheritance_Pemprograman Berorientasi Objek
+ * @author ACER_ELI DARUSTIYA_20210040068_TI21A
+ */
+
+public class Parent {
+    
+public class Child extends Parent{
+     int x;
+    public Child() {
+        x = 5;
+    }
+}
+
+}
