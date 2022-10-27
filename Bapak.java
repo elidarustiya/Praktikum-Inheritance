b30cd836c5755d312d1bd765a626ae4e4d816f68
ELI DARUSTIYA <114423143+elidarustiya@users.noreply.github.com>
2022-10-27 13:58:11 +0700

    Add files via upload

---
+++ Bapak.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Praktikum-Inheritance_Pemprograman Berorientasi Objek
+ * @author ACER_ELI DARUSTIYA_20210040068_TI21A
+ */
+
+public class Bapak {
+    int a;
+    int b;
+    
+public void show_variabel(){
+System.out.println("NIlai a="+ a);
+System.out.println("NIlai b="+ b);
+}
+}
