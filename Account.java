b942de895389245e22421ea3926785ca4b7d1b8d
ELI DARUSTIYA <114423143+elidarustiya@users.noreply.github.com>
2022-10-27 13:44:26 +0700

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Praktikum-Inheritance_Pemprograman Berorientasi Objek
+ * @author ACER_ELI DARUSTIYA_20210040068_TI21A
+ */
+
+public class Account {
+    protected double balance;
+    
+    public Account(double balance){
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+    
+    public boolean deposit(double amt){
+        balance+=amt;
+        return true;
+    }
+    
+    public boolean withDraw(double amt){
+        balance-=amt;
+        return true;
+    }
+}
