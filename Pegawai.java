5b42a35b8f7f1469c45d14afe1fe7baecbb33ffe
ELI DARUSTIYA <114423143+elidarustiya@users.noreply.github.com>
2022-10-27 14:46:54 +0700

    Add files via upload

---
+++ Pegawai.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Praktikum-Inheritance_Pemprograman Berorientasi Objek
+ * @author ACER_ELI DARUSTIYA_20210040068_TI21A
+ */
+
+public class Pegawai {
+    public String nama;
+    public double gaji;
+}
+
+class Manajer extends Pegawai {
+    public String departemen;
+    
+    public void IsiData(String n, String d) {
+        nama=n;
+        departemen=d;
+    }
+}
