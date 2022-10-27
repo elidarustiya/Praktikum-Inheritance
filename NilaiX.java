cf55898d3b1a779c309e7e73afc7e6986c4b46f1
ELI DARUSTIYA <114423143+elidarustiya@users.noreply.github.com>
2022-10-27 14:43:43 +0700

    Add files via upload

---
+++ NilaiX.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Praktikum-Inheritance_Pemprograman Berorientasi Objek
+ * @author ACER_ELI DARUSTIYA_20210040068_TI21A
+ */
+
+class Parent {
+    public int x = 5;
+}
+class Child extends Parent {
+    public void Info(int x) {
+        System.out.println("Nilai x sebagai parameter = " + x);
+        System.out.println("Data member x di class Child = " + this.x);
+        System.out.println("Data member x di class Parent = " + super.x);
+    }
+}
+public class NilaiX {
+    public static void main(String args[]) {
+        Child tes = new Child();
+        tes.Info(20);
+    }
+}
